1016ecc94c299f73b3e2de1f6def42df559d4f46
ClaudiaGarfias <me@claudiagarfias.works>
2019-06-03 19:08:32 -0500

    estructura html

---
+++ src/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <script type="text/javascript"src="app.js"> </script>
+    <link rel="stylesheet" href="/css/diseno.css">
+
+    <title>Cifrando al César</title>
+  </head>
+  <body>
+
+  </body>
+</html>
