f0ee4939dccd5c05a868d818d9310380b49424d5
ClaudiaGarfias <me@claudiagarfias.works>
2019-06-10 17:23:01 -0500

    guardarproyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,54 @@
 window.cipher = {
-  // ... 
+  encode:(offset,msj) => {
+   let resultado = "";
+    let mayusculas= msj.toUpperCase();
+
+for (let i=0;i<mayusculas.length;i++){
+  let textoconvertido= mayusculas.charCodeAt(i);
+  if (textoconvertido===32)
+  {
+   let cambioDePosicion= String.fromCharCode(textoconvertido);
+   resultado += cambioDePosicion;
+  }
+else{
+  let textoconvertido= mayusculas.charCodeAt(i);
+let offsetDos=  parseInt(offset);
+  let formula = (textoconvertido-65+offsetDos)%26+65;
+  let cambioDePosicion= String.fromCharCode(formula);
+  resultado += cambioDePosicion;
+}
+
+}
+
+return(resultado);
+ //  let textoconvertido= mayusculas.charCodeat();
+
+  },
+  decode:(offset,msj) => {
+    let resultado = "";
+     let mayusculas= msj.toUpperCase();
+
+ for (let i=0;i<mayusculas.length;i++){
+
+  let textoconvertido= mayusculas.charCodeAt(i);
+ if (textoconvertido===32)
+ {
+  let cambioDePosicion= String.fromCharCode(textoconvertido);
+  resultado += cambioDePosicion;
+ }
+else{
+  let textoconvertido= mayusculas.charCodeAt(i);
+ let offsetDos=  parseInt(offset);
+   let formula = (textoconvertido+65-offsetDos)%26+65;
+   let cambioDePosicion= String.fromCharCode(formula);
+   resultado += cambioDePosicion;
+}
+
+ }
+
+ return(resultado);
+  //  let textoconvertido= mayusculas.charCodeat();
+
+   },
+
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <script type="text/javascript"src="app.js"> </script>
-    <link rel="stylesheet" href="/css/diseno.css">
+let mensaje = document.getElementById("texto");
+let cifrar= document.getElementById("cifrar");
+let numero= document.getElementById("offset");
+let decifrar= document.getElementById("descifrar");
 
-    <title>Cifrando al César</title>
-  </head>
-  <body>
+cifrar.addEventListener("click",()=>{
+    let msj= mensaje.value;
+    let offset = numero.value;
+    let resultFinal = window.cipher.encode(offset,msj);
+    document.getElementById ("texto-cifrado").innerHTML= resultFinal;
+});
 
-  </body>
-</html>
+decifrar.addEventListener("click",()=>{
+    let msj= mensaje.value;
+    let offset = numero.value;
+    let resultFinal = window.cipher.decode(offset,msj);
+    document.getElementById ("decifrar-texto").innerHTML= resultFinal;
+});
